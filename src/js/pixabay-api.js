3477c905cd3a905b8dbc7d05721b829b8198ef63
kalops1a <vadymduchenko@gmail.com>
2024-04-01 01:23:44 +0200

    4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,52 +1,18 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-export function galleryHTTPRequest() {
-    document.getElementById(`searchForm`).addEventListener(`submit`, (evt) => {
-    evt.preventDefault();
+ 
 
-    const searchInputValue = document.getElementById(`searchInput`).value;
-    if (searchInputValue.trim() === ``) {
-        iziToast.alert({
-            title: `Alert`,
-            message: `Please enter a value.`,
-            position: `topCenter`
-        });
-        return;
+export async function fetchImages(searchQuery, apiKey) {
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error(`An error occurred while fetching data: ${error.message}`);
     }
-
-    const apiKey = `43152327-599555690163c4dbf744e6761`;
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.hits.length === 0) {
-                iziToast.info({
-                    title: `Info`,
-                    message: "Sorry, there are no images matching your search query. Please try again!",
-                    position: "topCenter"
-                });
-            } else {
-                
-                displayImages(data.hits);
-            }
-        })
-        .catch(error => {
-            iziToast.error({
-                title: `Error`,
-                message: 'An error occurred while fetching data. Please try again later.',
-                position: "topCenter"
-            });
-        });
-});
 }
-
-
 
 
 
